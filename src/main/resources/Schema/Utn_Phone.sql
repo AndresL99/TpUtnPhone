6e3aba28ad02be9ca2a5a3ad992f9b2d227f4e3d
Andres <andreslerner99@gmail.com>
2022-05-30 23:55:51 -0300

    Actualizacion de base de datos

--- src/main/resources/Schema/Utn_Phone.sql
+++ src/main/resources/Schema/Utn_Phone.sql
@@ -3,67 +3,73 @@ drop database utn_phones;
 create database Utn_Phones;
 use Utn_Phones;
 
+
 create table cities(
-name varchar(30),
-id_city bigint not null,
-prefix_number integer not null,
-constraint pk_id_city primary key (id_city)
+                       name varchar(30),
+                       id_city bigint not null,
+                       prefix_number integer not null,
+                       constraint pk_id_city primary key (id_city)
 );
 
 create table users(
-name varchar(30),
-surname varchar(30),
-dni integer not null,
-id_city bigint not null,
-province varchar(50) not null,
-constraint pk_dni primary key (dni),
-constraint fk_city foreign key(id_city) references cities(id_city)
+                      name varchar(30),
+                      surname varchar(30),
+                      dni integer not null,
+                      id_city bigint not null,
+                      province varchar(50) not null,
+                      username varchar(50) not null,
+                      is_employee bool default 0 not null,
+                      password varchar(50) not null,
+                      constraint pk_dni primary key (dni),
+                      constraint fk_city foreign key(id_city) references cities(id_city)
 );
 
 create table telephone_Lines(
-telephone_number integer not null,
-user_dni integer not null,
-constraint pk_telephone_number primary key (telephone_number),
-constraint fk_users foreign key(user_dni) references users(dni));
+                                telephone_number varchar(20) not null,
+                                user_dni integer not null,
+                                constraint pk_telephone_number primary key (telephone_number),
+                                constraint fk_users foreign key(user_dni) references users(dni));
 
 create table tariffs(
-id_tariff bigint not null,
-id_city_origin bigint not null,
-id_city_destination bigint not null,
-prece_x_minute float not null,
-begin_hour time not null,
-until_hour time not null,
-constraint pk_id_tariff primary key (id_tariff),
-constraint fk_city_origin foreign key(id_city_origin) references cities(id_city),
-constraint fk_city_destination foreign key(id_city_destination) references cities(id_city)
+                        id_tariff bigint not null,
+                        id_city_origin bigint not null,
+                        id_city_destination bigint not null,
+                        prece_x_minute float not null,
+                        begin_hour time not null,
+                        until_hour time not null,
+                        constraint pk_id_tariff primary key (id_tariff),
+                        constraint fk_city_origin foreign key(id_city_origin) references cities(id_city),
+                        constraint fk_city_destination foreign key(id_city_destination) references cities(id_city)
 );
 
 
+
 create table bills(
-id_bill bigint not null,
-user_dni integer not null,
-number_calls integer not null,
-price_total float not null,
-date_facturation date not null,
-date_expiration date not null,
-constraint pk_id_bill primary key (id_bill),
-constraint fk_user_dni foreign key(user_dni) references users(dni));
+                      id_bill bigint not null,
+                      user_dni integer not null,
+                      number_calls integer not null,
+                      price_total float not null,
+                      date_facturation date not null,
+                      date_expiration date not null,
+                      constraint pk_id_bill primary key (id_bill),
+                      constraint fk_user_dni foreign key(user_dni) references users(dni));
 
 create table calls(
-id_call bigint not null,
-id_number_origin integer not null,
-id_number_destination integer not null,
-id_city_origin bigint not null,
-id_city_destination bigint not null,
-id_tariff bigint not null,
-duration time not null,
-price_total float not null,
-id_bill bigint not null,
-constraint pk_id_call primary key (id_call),
-constraint fk_number_origin foreign key(id_number_origin) references telephone_Lines(telephone_number),
-constraint fk_number_destination foreign key(id_number_destination) references telephone_Lines(telephone_number),
-constraint fk_city_origin_call foreign key(id_city_origin) references cities(id_city),
-constraint fk_city_destination_call foreign key(id_city_destination) references cities(id_city),
-constraint fk_tariff foreign key(id_tariff) references tariffs(id_tariff),
-constraint fk_bill foreign key(id_bill) references bills(id_bill)
+                      id_call bigint auto_increment,
+                      id_number_origin varchar(20) ,
+                      id_number_destination varchar(20) ,
+                      id_city_origin bigint ,
+                      id_city_destination bigint ,
+                      id_tariff bigint ,
+                      day_time datetime ,
+                      duration bigint ,
+                      price_total float ,
+                      id_bill bigint ,
+                      constraint pk_id_call primary key (id_call),
+                      constraint fk_number_origin foreign key(id_number_origin) references telephone_Lines(telephone_number),
+                      constraint fk_number_destination foreign key(id_number_destination) references telephone_Lines(telephone_number),
+                      constraint fk_city_origin_call foreign key(id_city_origin) references cities(id_city),
+                      constraint fk_city_destination_call foreign key(id_city_destination) references cities(id_city),
+                      constraint fk_tariff foreign key(id_tariff) references tariffs(id_tariff),
+                      constraint fk_bill foreign key(id_bill) references bills(id_bill)
 );
---
+++ src/main/resources/Schema/script.sql
@@ -0,0 +1,27 @@
+insert into cities values ("Buenos Aires", 1,11);
+insert into cities values ("Mar del plata", 2,223);
+insert into cities values ("La Plata", 3,221);
+insert into cities values ("Pilar", 4,230);
+insert into cities values ("Tandil", 5,249);
+insert into cities values ("Mendoza", 6,260);
+insert into cities values ("San Juan", 7,264);
+insert into cities values ("Pilar", 8,230);
+insert into cities values ("Tandil", 9,249);
+insert into cities values ("Bahia Blanca", 10,291);
+insert into cities values ("Cordoba", 11,351);
+insert into cities values ("Santa Clara del Mar",12,2236);
+insert into cities values ("Villa Gesell",13,225);
+
+insert into users values ("nicolas", "roldan", 41306781, 2,"Buenos Aires",'nicoRoldan',1,'nico123');
+insert into users values ("Andres", "Lerner", 41458332, 1,"Buenos Aires",'andreslerner',1,'andres123');
+
+insert into telephone_Lines values("2235583444",41306781);
+insert into telephone_Lines values("1147475566",41458332);
+insert into telephone_Lines values("2254583444",41458332);
+
+insert into tariffs values (1,1,1,1.0,'15:00:00','20:00:00');
+insert into tariffs values (2,2,1,1.0,'12:00:00','23:59:59');
+insert into tariffs values (3,12,1,1.0,'12:00:00','23:59:59');
+insert into tariffs values (5,12,2,20.0,'12:00:00','23:59:59');
+insert into tariffs values (4,2,12,5.0,'12:00:00','23:59:59');
+
